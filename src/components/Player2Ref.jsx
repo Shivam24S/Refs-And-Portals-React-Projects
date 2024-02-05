ae507f1015dda82e2d5750eac5603e1c7db3a6a8
Shivam24S <shivamshrivastav8055@gmail.com>
2024-02-05 11:18:55 +0530

    conditional rendering

--- src/components/Player2Ref.jsx
+++ src/components/Player2Ref.jsx
@@ -12,7 +12,14 @@ export default function Player2Ref() {
 
   return (
     <section id="player">
-      <h2>Welcome {enteredName ? enteredName : "please enter your Name"} </h2>
+      {/* <h2>Welcome {enteredName ? enteredName : "please enter your Name"} </h2> */}
+
+      {/* here we can use another method also to conditional render in react */}
+
+      <h2>Welcome {enteredName ?? "please enter your name "} </h2>
+
+      {/* as you can see in upper code we want display the entered name on entered name so we can do like this and shortcut way */}
+
       <p>
         <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
